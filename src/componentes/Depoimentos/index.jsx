a75e57a290905de85bf4e1423b63211cbf573125
Thiago Reis <tchaaago@outlook.com>
2025-07-06 18:06:04 -0300

    componente dos cards finalizado e outros ajustes

--- src/componentes/Depoimentos/index.jsx
+++ src/componentes/Depoimentos/index.jsx
@@ -6,9 +6,9 @@ function CardsDepoimentos (){
     const [data, setData] = useState([]);
 
     useEffect (() => {
-    fetch('http://localhost:5173/json/Depoimentos.json')
-    .then((response) => response.json())
-    .then(setData);
+        fetch('/json/Depoimentos.json')
+            .then((response) => response.json())
+            .then(setData);
 }, []);
 
     if (!data || !data.length) return null;
@@ -16,13 +16,13 @@ function CardsDepoimentos (){
     return (
         <>
         <div className={styles.card}>
-            {data.map((depoimentos) => {
-                const {nome, perfil, depoimento } = depoimentos;
+            {data.map((depoimento, index) => {
+                const {nome, perfil, depoimento: texto } = depoimento;
 
                 return (
-                    <section className={styles.depoimentos} key={depoimento}>
+                    <section className={styles.depoimentos} key={index}>
                         <section className={styles.info}>
-                            <span className={styles.depoimento}>{depoimento}</span>
+                            <span className={styles.depoimento}>"{texto}"</span>
                             <h1 className={styles.nome}>{nome}</h1>
                             <p className={styles.perfil}>{perfil}</p>
                         </section>
