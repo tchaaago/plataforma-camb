119e40391f07c7fed99f59a2b5b2e29657a63ed0
Thiago Reis <tchaaago@outlook.com>
2025-07-13 22:28:20 -0300

    atualização dados-curriculares

--- src/services/dados-currisculares.js
+++ src/services/dados-currisculares.js
@@ -1,84 +1,101 @@
 export const dadosCurriculares = {
-  "1º Semestre": {
+   "1º Período": {
     chTotal: "416h",
-    chMinima: "400h",
+    chMinima: "360h",
     disciplinas: [
-      { codigo: "CB0704", nome: "CÁLCULO DIFERENCIAL", ch: "64H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB0704", nome: "ELEMENTOS DE FÍSICA I", ch: "64H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB0704", nome: "ÉTICA I", ch: "64H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB0704", nome: "ECOLOGIA APLICADA À CIÊNCIAS AMBIENTAIS", ch: "96H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB0704", nome: "QUÍMICA APLICADA À CIÊNCIAS AMBIENTAIS", ch: "64H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB0704", nome: "GEOLOGIA APLICADA À CIÊNCIAS AMBIENTAIS", ch: "96H", natureza: "OBRIGATÓRIA" },
+      { codigo: "CB0704", nome: "CÁLCULO FUNDAMENTAL I", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "CD0501", nome: "ELEMENTOS DE FÍSICA I", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "ICA1612", nome: "ÉTICA I", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0001", nome: "ECOLOGIA APLICADA AS CIÊNCIAS AMBIENTAIS", ch: "96H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0003", nome: "QUÍMICA APLICADA AS CIÊNCIAS AMBIENTAIS", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0005", nome: "GEOLOGIA APLICADA AS CIÊNCIAS AMBIENTAIS", ch: "64H", natureza: "OBRIGATÓRIA" },
     ],
   },
-  "2º Semestre": {
+  "2º Período": {
     chTotal: "384h",
     chMinima: "360h",
     disciplinas: [
-      { codigo: "CB0801", nome: "CÁLCULO INTEGRAL", ch: "64H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB0802", nome: "BIOQUÍMICA", ch: "64H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB0803", nome: "CARTOGRAFIA E GEOPROCESSAMENTO", ch: "96H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB0804", nome: "ESTATÍSTICA APLICADA", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "ICA1615", nome: "FILOSOFIA DA NATUREZA", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0002", nome: "INTRODUÇÃO A ECONOMIA AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0004", nome: "CLIMATOLOGIA E MUDANÇAS GLOBAIS", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0006", nome: "ECOSSISTEMAS AQUÁTICOS CONTINENTAIS", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0007", nome: "GEOMORFOLOGIA APLICADA", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0050", nome: "POLUIÇÃO AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
     ],
   },
-  "3º Semestre": {
-    chTotal: "400h",
-    chMinima: "380h",
+  "3º Período": {
+    chTotal: "384h",
+    chMinima: "360h",
     disciplinas: [
-        { codigo: "CB0901", nome: "MICROBIOLOGIA AMBIENTAL", ch: "80H", natureza: "OBRIGATÓRIA" },
-        { codigo: "CB0902", nome: "ECONOMIA AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
-        { codigo: "CB0903", nome: "DIREITO AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "HD0957", nome: "INTRODUÇÃO À SOCIOLOGIA", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0009", nome: "RECURSOS HÍDRICOS", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0010", nome: "ECOSSISTEMAS MARINHOS", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0011", nome: "TRATAMENTO DE DADOS AMBIENTAIS", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0013", nome: "GEOPROCESSAMENTO E SENSORIAMENTO REMOTO", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0051", nome: "POLUIÇÃO DE AMBIENTES AQUÁTICOS", ch: "64H", natureza: "OBRIGATÓRIA" },
     ],
   },
-  "4º Semestre": {
-    chTotal: "420h",
-    chMinima: "400h",
+  "4º Período": {
+    chTotal: "384h",
+    chMinima: "360h",
     disciplinas: [
-      { codigo: "CB1001", nome: "HIDROLOGIA", ch: "80H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB1002", nome: "CLIMATOLOGIA", ch: "64H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB1003", nome: "GESTÃO AMBIENTAL", ch: "96H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0014", nome: "HIDRODINÂMICA DE AMBIENTES AQUÁTICOS", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0016", nome: "VALORAÇÃO AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0018", nome: "SIG - SISTEMAS DE INFORMAÇÕES GEOGRÁFICAS", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0052", nome: "POLUIÇÃO ATMOSFÉRICA", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0085", nome: "MICROBIOLOGIA AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0094", nome: "ANÁLISE E PLANEJAMENTO AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
     ],
   },
-  "5º Semestre": {
-    chTotal: "360h",
-    chMinima: "340h",
+  "5º Período": {
+    chTotal: "256h",
+    chMinima: "360h",
     disciplinas: [
-      { codigo: "CB1101", nome: "AVALIAÇÃO DE IMPACTOS AMBIENTAIS", ch: "96H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB1102", nome: "LEGISLAÇÃO AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0019", nome: "MODELAGEM AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0023", nome: "INSTRUMENTAÇÃO NO MONITORAMENTO AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0059", nome: "DIREITO AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0095", nome: "IMPACTOS AMBIENTAIS", ch: "64H", natureza: "OBRIGATÓRIA" },
     ],
   },
-  "6º Semestre": {
-    chTotal: "390h",
-    chMinima: "370h",
+  "6º Período": {
+    chTotal: "192h",
+    chMinima: "360h",
     disciplinas: [
-      { codigo: "CB1201", nome: "ENGENHARIA SANITÁRIA E AMBIENTAL", ch: "96H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB1202", nome: "TÓPICOS ESPECIAIS EM ENERGIA", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0024", nome: "CERTIFICAÇÃO E AUDITORIA AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0096", nome: "EDUCAÇÃO AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0097", nome: "TRATAMENTO E REUSO DE EFLUENTES LÍQUIDOS", ch: "64H", natureza: "OBRIGATÓRIA" },
     ],
   },
-  "7º Semestre": {
-    chTotal: "450h",
-    chMinima: "430h",
+  "7º Período": {
+    chTotal: "128h",
+    chMinima: "360h",
     disciplinas: [
-      { codigo: "CB1301", nome: "PLANEJAMENTO E GESTÃO DE RECURSOS HÍDRICOS", ch: "96H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB1302", nome: "RECUPERAÇÃO DE ÁREAS DEGRADADAS", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0025", nome: "RECUPERAÇÃO DE ÁREAS DEGRADADAS", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0048", nome: "TRABALHO DE CONCLUSÃO DE CURSO 1", ch: "64H", natureza: "OBRIGATÓRIA" },
     ],
   },
-  "8º Semestre": {
-    chTotal: "480h",
-    chMinima: "460h",
+  "8º Período": {
+    chTotal: "704h",
+    chMinima: "360h",
     disciplinas: [
-      { codigo: "CB1401", nome: "TRABALHO DE CONCLUSÃO DE CURSO I", ch: "96H", natureza: "OBRIGATÓRIA" },
-      { codigo: "CB1402", nome: "ESTÁGIO SUPERVISIONADO", ch: "128H", natureza: "OBRIGATÓRIA" },
+      { codigo: "CAMB0002", nome: "ATIVIDADES COMPLEMENTARES", ch: "112H", natureza: "OBRIGATÓRIA" },
+      { codigo: "EXT0083", nome: "UNIDADE CURRICULAR ESPECIAL DE EXTENSÃO", ch: "272H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0027", nome: "RISCOS E PERÍCIA AMBIENTAL", ch: "64H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0028", nome: "ESTÁGIO SUPERVISIONADO - CIÊNCIAS AMBIENTAIS", ch: "192H", natureza: "OBRIGATÓRIA" },
+      { codigo: "LAB0049", nome: "TRABALHO DE CONCLUSÃO DE CURSO 2", ch: "64H", natureza: "OBRIGATÓRIA" },
     ],
   },
   "Optativas": {
     chTotal: "Variável",
     chMinima: "N/A",
     disciplinas: [
-        { codigo: "OP001", nome: "TÓPICOS ESPECIAIS EM ENERGIAS RENOVÁVEIS", ch: "64H", natureza: "OPTATIVA" },
-        { codigo: "OP002", nome: "GESTÃO DE RESÍDUOS SÓLIDOS", ch: "64H", natureza: "OPTATIVA" },
-        { codigo: "OP003", nome: "BIODIVERSIDADE E CONSERVAÇÃO", ch: "64H", natureza: "OPTATIVA" },
-        { codigo: "OP004", nome: "EDUCAÇÃO AMBIENTAL", ch: "64H", natureza: "OPTATIVA" },
+      { codigo: "HLL0077", nome: "LÍNGUA BRASILEIRA DE SINAIS - LIBRAS", ch: "64H", natureza: "OPTATIVA" },
+      { codigo: "LAB0046", nome: "BIOTECNOLOGIA AMBIENTAL", ch: "64H", natureza: "OPTATIVA" },
+      { codigo: "LAB0054", nome: "GESTÃO DE BACIAS HIDROGRÁFICAS", ch: "64H", natureza: "OPTATIVA" },
+      { codigo: "LAB0106", nome: "LICENCIAMENTO AMBIENTAL", ch: "64H", natureza: "OPTATIVA" },
+      { codigo: "TD0961", nome: "GESTÃO DE RESÍDUOS SÓLIDOS", ch: "48H", natureza: "OPTATIVA" },
+      { codigo: "VE0048", nome: "MANEJO DE ECOSSISTEMAS MARINHOS", ch: "48H", natureza: "OPTATIVA" },
+      { codigo: "PC0353", nome: "EDUCAÇÃO EM DIREITOS HUMANOS", ch: "64H", natureza: "OPTATIVA" },
     ],
-  }
+  },
 };
